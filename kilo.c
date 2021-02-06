77de20718a8546cb12d85dab7be6f6af2523f678
imamuray <imamuray1313@gmail.com>
2021-02-06 11:21:55 +0900

    step6: Disable raw mode at exit

--- kilo.c
+++ kilo.c
@@ -1,16 +1,26 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
-void enableRawMode() {
-  struct termios raw;
+struct termios orig_termios;
 
-  tcgetattr(STDIN_FILENO, &raw);
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
 
   // c_flag: local flag
   // ECHO フラグを反転させて論理積を取っている
   // → echo しないようにしてる
   raw.c_lflag &= ~(ECHO);
 
+  // TCSAFLUSH: fd に書き込まれたすべての出力が端末に送信された後で変更が実行される.
+  // 加えて、受信したが読まれなかった入力は捨てられる.
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
