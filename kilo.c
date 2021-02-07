fa9b83e2793dbde3a244fc72180621a17c6caa92
imamuray <imamuray1313@gmail.com>
2021-02-07 14:02:31 +0900

    step13,14: Turn off all output processing

--- kilo.c
+++ kilo.c
@@ -16,22 +16,28 @@ void enableRawMode() {
 
   struct termios raw = orig_termios;
 
-  // c_iflag: input
-  // IXON: 出力の XON/XOFF フロー制御を有効にする
-  // ICRNL: 入力の CR('\r') を NL('\n') に置き換える
-  // → ctrl-s(XON), ctrl-q(XOFF) を無効にする
-  // → ctrl-m が変換されずに 13 と表示される
-  // c_flag: local flag
-  // ECHO: 入力をエコーする
-  // ICANON: 入力を行単位に設定
-  // IEXTEN: 実装依存の入力処理を有効にする
-  // ISIG: INTR, QUIT, SUSP, DSUSP の文字を受信した時、対応するシグナルを 発生させる
-  // これらのフラグの論理和を取って否定する
-  // → echo しないようにしてる
-  // → 入力はバイト単位
-  // → ctrl-x を入力できるようにする
-  // → シグナルが発生しなくなる、つまりctrl-c, ctrl-z が入力されても動く
+  /*
+   * c_iflag: input
+   * IXON: 出力の XON/XOFF フロー制御を有効にする
+   * ICRNL: 入力の CR('\r') を NL('\n') に置き換える
+   * → ctrl-s(XON), ctrl-q(XOFF) を無効にする
+   * → ctrl-m が変換されずに 13 と表示される
+   * c_oflag: output
+   * OPOST: 実装に依存した出力処理を有効にする
+   * → 無効にする
+   * c_flag: local flag
+   * ECHO: 入力をエコーする
+   * ICANON: 入力を行単位に設定
+   * IEXTEN: 実装依存の入力処理を有効にする
+   * ISIG: INTR, QUIT, SUSP, DSUSP の文字を受信した時、対応するシグナルを 発生させる
+   * これらのフラグの論理和を取って否定する
+   * → echo しないようにしてる
+   * → 入力はバイト単位
+   * → ctrl-x を入力できるようにする
+   * → シグナルが発生しなくなる、つまりctrl-c, ctrl-z が入力されても動く
+   */
   raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_oflag &= ~(OPOST);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
   // TCSAFLUSH: fd に書き込まれたすべての出力が端末に送信された後で変更が実行される.
@@ -46,10 +52,10 @@ int main() {
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
     if (iscntrl(c)) {
       // control charactor: ASCII code 0-31, 127
-      printf("%03d\n", c);
+      printf("%03d\r\n", c);
     } else {
       // ASCII code 32-126
-      printf("%03d ('%c')\n", c, c);
+      printf("%03d ('%c')\r\n", c, c);
     }
   }
   return 0;
