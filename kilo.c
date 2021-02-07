ae0056d0ec0a8fc63133cc6d63ba00e8406f28e9
imamuray <imamuray1313@gmail.com>
2021-02-07 13:51:16 +0900

    step12: Fix ctrl-m

--- kilo.c
+++ kilo.c
@@ -18,7 +18,9 @@ void enableRawMode() {
 
   // c_iflag: input
   // IXON: 出力の XON/XOFF フロー制御を有効にする
+  // ICRNL: 入力の CR('\r') を NL('\n') に置き換える
   // → ctrl-s(XON), ctrl-q(XOFF) を無効にする
+  // → ctrl-m が変換されずに 13 と表示される
   // c_flag: local flag
   // ECHO: 入力をエコーする
   // ICANON: 入力を行単位に設定
@@ -29,7 +31,7 @@ void enableRawMode() {
   // → 入力はバイト単位
   // → ctrl-x を入力できるようにする
   // → シグナルが発生しなくなる、つまりctrl-c, ctrl-z が入力されても動く
-  raw.c_iflag &= ~(IXON);
+  raw.c_iflag &= ~(ICRNL | IXON);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
   // TCSAFLUSH: fd に書き込まれたすべての出力が端末に送信された後で変更が実行される.
