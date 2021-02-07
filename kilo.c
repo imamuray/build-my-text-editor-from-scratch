3ee40bd146b107c0f855e6fa2a718e85c4b3bb50
imamuray <imamuray1313@gmail.com>
2021-02-07 13:11:12 +0900

    step8: Display keypresses

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -31,6 +33,14 @@ int main() {
   enableRawMode();
 
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      // control charactor: ASCII code 0-31, 127
+      printf("%03d\n", c);
+    } else {
+      // ASCII code 32-126
+      printf("%03d ('%c')\n", c, c);
+    }
+  }
   return 0;
 }
