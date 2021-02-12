303c40cf9b5d046d54ff41bf3b6a5fc5e33be6f5
imamuray <imamuray1313@gmail.com>
2021-02-13 02:08:45 +0900

    step46-48: Arrow keys

--- kilo.c
+++ kilo.c
@@ -21,6 +21,13 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN
+};
+
 /*** data ***/
 
 struct editorConfig {
@@ -99,13 +106,30 @@ void enableRawMode() {
 /**
  * ターミナルから1文字読み取る
  */
-char editorReadkey() {
+int editorReadkey() {
   int nread;
   char c;
   while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
     if (nread == -1 && errno != EAGAIN) die("read");
   }
-  return c;
+
+  if (c != '\x1b') {
+    return c;
+  } else {
+    char seq[3];
+    if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+    if (seq[0] == '[') {
+      switch (seq[1]) {
+        case 'A': return ARROW_UP;
+        case 'B': return ARROW_DOWN;
+        case 'C': return ARROW_RIGHT;
+        case 'D': return ARROW_LEFT;
+      }
+    }
+    return '\x1b';
+  }
 }
 
 /**
@@ -266,18 +290,18 @@ void editorRefreshScreen() {
 
 /*** input ***/
 
-void editorMoveCursor(char key) {
+void editorMoveCursor(int key) {
   switch (key) {
-    case 'h':
+    case ARROW_LEFT:
       E.cursorX--;
       break;
-    case 'l':
+    case ARROW_RIGHT:
       E.cursorX++;
       break;
-    case 'k':
+    case ARROW_UP:
       E.cursorY--;
       break;
-    case 'j':
+    case ARROW_DOWN:
       E.cursorY++;
       break;
   }
@@ -287,7 +311,7 @@ void editorMoveCursor(char key) {
  * 押したキーによって動作を分岐させる
  */
 void editorProcessKeypress() {
-  char c = editorReadkey();
+  int c = editorReadkey();
 
   switch (c) {
     case CTRL_KEY('q'):
@@ -296,10 +320,10 @@ void editorProcessKeypress() {
       exit(0);
       break;
     
-    case 'h':
-    case 'l':
-    case 'k':
-    case 'j':
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
+    case ARROW_UP:
+    case ARROW_DOWN:
       editorMoveCursor(c);
       break;
   }
